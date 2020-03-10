430941063c95d5d30bff69a48ae766e8f3f6d343
Josh Stafford <josh@jdstafford.com>
2020-03-10 00:15:18 -0400

    stubbing router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {
-    Route,
-    Switch
-} from 'react-router-dom';
-import {fakeInterests, fakeSkills} from "./dummy-data";
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { fakeInterests, fakeSkills } from "./dummy-data";
+import Login from "./Login";
 
 function App() {
-    console.log('test return', fakeInterests);
-    console.log('test return', fakeSkills);
+  console.log("test return", fakeInterests);
+  console.log("test return", fakeSkills);
   return (
     <div className="App">
-      <p>This is App.js</p>
+      <Router>
         <Switch>
-
+          <Route path="/login">
+            <Login />
+          </Route>
         </Switch>
+      </Router>
     </div>
   );
 }

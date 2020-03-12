0cfd6eae9e6b6926213ebd29d2d1ab96b01bc108
Josh Stafford <josh@jdstafford.com>
2020-03-12 01:25:22 -0400

    styling

--- src/Home/HomePage.js
+++ src/Home/HomePage.js
@@ -15,8 +15,18 @@ function HomePage() {
     }, [dispatch, user.id]);
 
     return (
-        <div className="HomePage">
+        <div className="HomePage container">
             <h1>Welcome {user.name}</h1>
+
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+                euismod bibendum laoreet. Proin gravida dolor sit amet lacus
+                accumsan et viverra justo commodo. Proin sodales pulvinar sic
+                tempor. Sociis natoque penatibus et magnis dis parturient
+                montes, nascetur ridiculus mus. Nam fermentum, nulla luctus
+                pharetra vulputate, felis tellus mollis orci, sed rhoncus pronin
+                sapien nunc accuan eget.
+            </p>
 
             <h2>Interests</h2>
             <div className="columns is-flex is-mobile">

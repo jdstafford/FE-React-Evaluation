b3dbd9ccd0b8f728c3671ff4af897e0f24e7e19f
Josh Stafford <josh@jdstafford.com>
2020-03-16 09:50:26 -0400

    form validation

--- src/pages/Login/LoginForm.js
+++ src/pages/Login/LoginForm.js
@@ -10,40 +10,60 @@ function LoginForm() {
         password: ''
     });
     const [isLoading, setIsLoading] = useState('');
+
+    const [errorMessages, setErrorMessages] = useState({});
     const { username, password } = inputValues;
 
     function handleChange(e) {
         const { name, value } = e.target;
+
         setInputValues(inputValues => ({ ...inputValues, [name]: value }));
     }
 
     function handleSubmit(e) {
         e.preventDefault();
-
         if (username && password) {
             setIsLoading('is-loading');
             dispatch(userActions.login(username, password));
+        } else {
+            setErrorMessages({
+                username: username ? '' : 'Username is required',
+                password: password ? '' : 'Password is required'
+            });
         }
     }
 
     return (
         <form name="loginForm" onSubmit={handleSubmit}>
+            {errorMessages.username && (
+                <span className="has-text-danger is-size-7">
+                    Username is required.
+                </span>
+            )}
             <input
-                className="input is-radiusless"
+                className={`input is-radiusless ${
+                    errorMessages.username ? 'is-danger' : ''
+                }`}
                 name="username"
                 placeholder="Username"
-                onChange={handleChange}
                 value={username}
-                required
+                onChange={handleChange}
             />
+
+            {errorMessages.password && (
+                <span className="has-text-danger is-size-7">
+                    Password is required.
+                </span>
+            )}
             <input
-                className="input is-radiusless"
+                className={`input is-radiusless ${
+                    errorMessages.password ? 'is-danger' : ''
+                }`}
                 name="password"
                 type="password"
                 placeholder="Password"
-                onChange={handleChange}
                 value={password}
-                required
+                onChange={handleChange}
             />
 
             <button className={`button ${isLoading}`}>LOGIN</button>
